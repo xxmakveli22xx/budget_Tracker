073148fc733f1cd51ac08debf7e27f1a4cc18891
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-11-25 11:28:14 -0600

    updated the addeventlistener

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { json } = require("express");
+
 const indexedDB = window.indexedDB || window.mozIndexedDB;
 
 let db;
@@ -28,7 +30,29 @@ function getDataBase(){
     const transaction = db.transaction(["pending"], "readwrite")
     const storage = transaction.objectStore("pending");
     const getall = storage.getAll();
+    getall.onsuccess = function(){
+        if(getall.result.length > 0){
+            fetch("/api/transaction/bulk", {
+                method:"POST", 
+                body: json.stringify(getall.result),
+                headers:{
+                   Accept: "application/json, text/plain",
+                   "Content-Type": "application/json" 
+                }
+            }).then(function (response){
+                return response.json();
+            }).then(function(){
+                const transaction = db.transaction(["pending"], "readwrite")
+                const storage = transaction.objectStore("pending");
+                storage.clear();
+                
 
+
+            })
+
+        }
+    } 
 
 
 }
+window.addEventListener("Online", getDataBase);
