ebf3aac8014009a183bcb18aa34df681116e175c
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-11-25 11:04:23 -0600

    updated db.js

--- public/db.js
+++ public/db.js
@@ -5,4 +5,12 @@ const request = indexedDB.open("budgetTracker", 1)
 
 request.onupgradeneeded = ({target}) => {
     let db = target.result;
+    db.createObjectStore("working", {autoIncrement: true});
+
+}
+request.onsuccess = ({target}) => {
+    let db = target.result;
+    if(navigator.onLine){
+        checkDatabase()
+    }
 }
